22ecfef9b52e2319f3a8ed8d91a3ae2fd1a269fa
Harjit Brar <workandresearchbrar@gmail.com>
2024-09-05 17:13:05 +0800

    feat: add job config and job launcher

---
+++ toy-log-validator/src/main/java/com/project/toy_log_validator/config/ValidatorConfig.java
@@ -0,0 +1,65 @@
+package com.project.toy_log_validator.config;
+
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import com.project.toy_log_validator.tasks.ValidateDataTasklet;
+import com.project.toy_log_validator.tasks.ValidateSchemaTasklet;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Configuration
+@ComponentScan("com.project.toy_log_validator.tasks")
+public class ValidatorConfig {
+    @Autowired
+    private ValidateSchemaTasklet schemaTasklet;
+
+    @Autowired
+    private ValidateDataTasklet dataTasklet;
+
+    @Autowired
+
+    @Bean("stepValidationSchema")
+    public Step validateSchema(@Autowired JobRepository repository,
+            @Autowired PlatformTransactionManager transactionManager) {
+        log.info("Creating step (validation schema)...");
+
+        return new StepBuilder("stepValidationSchema", repository)
+                .tasklet(schemaTasklet, transactionManager)
+                .build();
+    }
+
+    @Bean("stepValidationData")
+    public Step validateData(@Autowired JobRepository repository,
+            @Autowired PlatformTransactionManager transactionManager) {
+        log.info("Creating step (validation data)...");
+
+        return new StepBuilder("stepValidationData", repository)
+                .tasklet(dataTasklet, transactionManager)
+                .build();
+    }
+
+    @Bean("validation")
+    public Job validateJob(@Autowired JobRepository repository,
+            @Autowired @Qualifier("stepValidationSchema") Step validateSchemaStep,
+            @Autowired @Qualifier("stepValidationData") Step validateDataStep) {
+        log.info("Creating job (validation)...");
+
+        return new JobBuilder("validation", repository)
+                .incrementer(new RunIdIncrementer())
+                .start(validateSchemaStep)
+                .next(validateDataStep)
+                .build();
+    }
+}
---
+++ toy-log-validator/src/main/java/com/project/toy_log_validator/config/ValidatorLauncherConfig.java
@@ -0,0 +1,63 @@
+package com.project.toy_log_validator.config;
+
+import org.apache.kafka.common.Uuid;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import com.opencsv.CSVReader;
+import com.project.toy_log_validator.entity.Reports;
+import com.project.toy_log_validator.service.ReportService;
+import com.project.toy_log_validator.utils.WorkbookUtil;
+
+import lombok.extern.slf4j.Slf4j;
+import java.math.BigInteger;
+
+@Slf4j
+@Configuration
+@EnableScheduling
+public class ValidatorLauncherConfig {
+    @Autowired
+    private ReportService reportService;
+
+    @Autowired
+    private JobLauncher jobLauncher;
+
+    @Autowired
+    private Job job;
+
+    @Scheduled(cron = "*/10 * * * * ?")
+    public void runBatchJob() {
+        try {
+            String uuid = Uuid.randomUuid().toString();
+            Reports reports = reportService.getLatestReport(uuid);
+            BigInteger playerId = reports.getPlayerId();
+            String reportId = reports.getReportId();
+
+            CSVReader report = reportService.getReportCsv(reports.getReportId(), uuid);
+            int[] sizes = WorkbookUtil.getDimensions(report);
+
+            if (!reports.getIsProcessed()) {
+                JobParameters parameter = new JobParametersBuilder()
+                        .addString("uuid", uuid)
+                        .addString("reportId", reportId)
+                        .addLong("playerId", playerId.longValue())
+                        .addLong("timestamp", System.currentTimeMillis())
+                        .addDouble("colSize", sizes[0] + 0.0)
+                        .addDouble("rowSize", sizes[1] + 0.0)
+                        .toJobParameters();
+
+                log.info("Job Parameters: {}", parameter);
+
+                jobLauncher.run(job, parameter);
+            }
+        } catch (Exception e) {
+            log.error("error: {}", e.getMessage());
+        }
+    }
+}
