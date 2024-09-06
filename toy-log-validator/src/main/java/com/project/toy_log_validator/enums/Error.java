54935d3bd46d670220a45ec455e58ae3a2a290e3
Harjit Brar <workandresearchbrar@gmail.com>
2024-09-06 16:46:13 +0800

    feat: add more exception handling

--- toy-log-validator/src/main/java/com/project/toy_log_validator/enums/Error.java
+++ toy-log-validator/src/main/java/com/project/toy_log_validator/enums/Error.java
@@ -10,7 +10,8 @@ public enum Error {
     DATA_PRC_ERROR("DATA-PERC-01", "Given value must follow percentage formula"),
     DATA_DMG_ERROR("DATA-PERC-02", "Given value must be in numeric"),
     DATA_VAL_ERROR("DATA-PERC-03", "Given value must be in string format"),
-    DATA_EMP_ERROR("DATA-EMP-04", "Given value must not be null or blank");
+    DATA_EMP_ERROR("DATA-EMP-04", "Given value must not be null or blank"),
+    REPORT_NULL_ERROR("RPRT-EMP-01", "Given report does not exist in the database");
 
     private final String code;
     private final String message;
