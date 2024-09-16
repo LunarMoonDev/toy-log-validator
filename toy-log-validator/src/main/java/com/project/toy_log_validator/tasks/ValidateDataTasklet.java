0d1ad3debb98dca018ac8472b3676f18b14b5f3e
Harjit Brar <workandresearchbrar@gmail.com>
2024-09-16 17:57:47 +0800

    refactor: fix error handling in tasklet

--- toy-log-validator/src/main/java/com/project/toy_log_validator/tasks/ValidateDataTasklet.java
+++ toy-log-validator/src/main/java/com/project/toy_log_validator/tasks/ValidateDataTasklet.java
@@ -1,5 +1,7 @@
 package com.project.toy_log_validator.tasks;
 
+import java.io.IOException;
+
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.scope.context.ChunkContext;
@@ -10,7 +12,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
 import com.project.toy_log_validator.enums.Validation;
+import com.project.toy_log_validator.exceptions.GenericException;
+import com.project.toy_log_validator.exceptions.ValidationException;
 import com.project.toy_log_validator.service.DataValidatorService;
 import com.project.toy_log_validator.service.ReportService;
 
@@ -34,18 +39,16 @@ public class ValidateDataTasklet implements Tasklet {
     private String fileId;
 
     @Override
-    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws ValidationException, CsvValidationException, IOException {
         log.info("X-Tracker: {} | task one: data validation", uuid);
 
-        CSVReader csvReport = reportService.getReportCsv(fileId, uuid);
-        Validation response = validatorService.dataValidation(csvReport, uuid);
-
-        if(response.equals(Validation.PASS)) {
-            return RepeatStatus.FINISHED;
-        } else {
-            // TODO: push a message to a kafka topic
+        try {
+            CSVReader csvReport = reportService.getReportCsv(fileId, uuid);
+            Validation response = validatorService.dataValidation(csvReport, uuid);
 
             return RepeatStatus.FINISHED;
+        } catch (GenericException exception) {
+            throw new ValidationException(exception.getError(), fileId, uuid);
         }
     }
 }
