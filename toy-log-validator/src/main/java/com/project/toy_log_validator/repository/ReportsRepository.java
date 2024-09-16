2bf6020644dedb2d4ace5468b64c270bda52c72f
Harjit Brar <workandresearchbrar@gmail.com>
2024-09-16 17:55:53 +0800

    feat: add method to get non-processed and latest reports from repository

--- toy-log-validator/src/main/java/com/project/toy_log_validator/repository/ReportsRepository.java
+++ toy-log-validator/src/main/java/com/project/toy_log_validator/repository/ReportsRepository.java
@@ -3,6 +3,7 @@ package com.project.toy_log_validator.repository;
 import java.math.BigInteger;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import org.springframework.transaction.annotation.Transactional;
@@ -12,6 +13,8 @@ import com.project.toy_log_validator.entity.Reports;
 @Repository
 @Transactional
 public interface ReportsRepository extends JpaRepository<Reports, BigInteger> {
+    @Query(value = "SELECT * FROM reports WHERE is_processed = '0' AND is_error = '0' ORDER BY gmt_create LIMIT 1", nativeQuery = true)
     Reports findFirstByOrderByGmtCreateDesc();
+
     Optional<Reports> findByReportId(String reportId);
 }
